66a58473d9f393a700b2c0cbaf27518a5c172526
lokeshdawar <lokesh.dawar@gmail.com>
2017-07-17 01:31:41 +0530

    clients....

---
+++ springDataHelloWorld/eureka-client-iteminfo/src/main/java/org/lucksoft/eurekaclientiteminfo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.lucksoft.eurekaclientiteminfo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EurekaClientIteminfoApplication.class);
+	}
+
+}
---
+++ springDataHelloWorld/eureka-client-itemreview/src/test/java/org/lucksoft/eurekaclientitemreview/EurekaClientItemreviewApplicationTests.java
@@ -0,0 +1,16 @@
+package org.lucksoft.eurekaclientitemreview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaClientItemreviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
