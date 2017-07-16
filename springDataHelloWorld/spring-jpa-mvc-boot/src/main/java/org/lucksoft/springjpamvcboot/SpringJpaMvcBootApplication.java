762d74647338666a50048dda0d1855847732260d
lokeshdawar <lokesh.dawar@gmail.com>
2017-07-16 13:20:53 +0530

    added eureka server and client

--- springDataHelloWorld/spring-jpa-mvc-boot/src/main/java/org/lucksoft/springjpamvcboot/SpringJpaMvcBootApplication.java
+++ springDataHelloWorld/spring-jpa-mvc-boot/src/main/java/org/lucksoft/springjpamvcboot/SpringJpaMvcBootApplication.java
@@ -2,8 +2,10 @@ package org.lucksoft.springjpamvcboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringJpaMvcBootApplication {
 
 	public static void main(String[] args) {
